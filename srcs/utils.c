d3fa146bf951c8a42d6a58a238327f6d270e37fe
Thiwanka Somachandra <thivankas@outlook.com>
2025-05-07 11:28:48 +0300

    Re-factoring and re-arranging the coe for norm stds

--- srcs/utils.c
+++ srcs/utils.c
@@ -1,35 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/07 11:16:31 by tsomacha          #+#    #+#             */
+/*   Updated: 2025/05/07 11:24:32 by tsomacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/shell.h"
 
-char *ft_strnmdup(char const *src, int n , int m)
+/**
+ * Function declaration
+*/
+int		ft_strnmcpy(char **dest, char *src, int n, int m);
+int		ft_arraylen(char **envp);
+int		ft_isempty(char *str);
+int		ft_isspace(int c);
+char	*ft_strnmdup(char const *src, int n, int m);
+
+/*
+* Copies a substring from `src` between indices `n` and `m` into
+* a newly allocated string.
+*
+* Parameters:
+* - dest: A pointer to the destination string pointer (will be allocated).
+* - src: The source string.
+* - n: The starting index (inclusive).
+* - m: The ending index (exclusive).
+*
+* Returns:
+* - The number of characters copied on success, or -1 on error.
+*/
+
+int	ft_strnmcpy(char **dest, char *src, int n, int m)
 {
-	int i = 0;
-	char *dest;
+	int	i;
 
-	if(!src || m == 0)
-		return (NULL);
-	dest = malloc(sizeof(char) * ((m - n) + 1));
-	if(!dest)
-		return (NULL);
-	while(src && src[i + n] && (i + n) < m)
-	{
-		dest[i] = src[i + n];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-int ft_strnmcpy(char **dest, char *src, int n, int m)
-{
-	int i = 0;
-
+	i = 0;
 	if (n >= m || !src || !dest)
 		return (-1);
-
 	*dest = malloc(sizeof(char) * (m - n + 1));
 	if (!*dest)
 		return (-1);
-
 	while ((i + n) < m && src[i + n])
 	{
 		(*dest)[i] = src[i + n];
@@ -39,39 +54,15 @@ int ft_strnmcpy(char **dest, char *src, int n, int m)
 	return (i);
 }
 
-void print_args(char **args, int size)
-{
-	int i = 0;
-	while(i <= size)
-	{
-		printf("args[%d] : %s\n", i, args[i]);
-		i++;
-	}
-}
-
-void print_cmd(t_cmd *cmd)
-{
-	t_cmd *current = cmd;
-	while(current)
-	{
-		printf("command : %s\n", current->command);
-		printf("args : \n");
-		print_args(current->args, current->num_args);
-		current = current->next;
-	}
-}
-
-void print(t_list *list, char *msg)
-{
-	printf("\n%s\n", msg);
-	t_list *current = list;
-	while(current)
-	{
-		printf("%s\n", current->token);
-		current = current->next;
-	}
-	printf("------------------------------------\n");
-}
+/*
+* Calculates the number of strings in a NULL-terminated string array.
+*
+* Parameters:
+* - envp: The string array (e.g., environment variables).
+*
+* Returns:
+* - The number of elements in the array.
+*/
 
 int	ft_arraylen(char **envp)
 {
@@ -82,6 +73,16 @@ int	ft_arraylen(char **envp)
 		len++;
 	return (len);
 }
+
+/*
+* Checks if a string is empty or consists only of whitespace characters.
+*
+* Parameters:
+* - str: The input string.
+*
+* Returns:
+* - 1 if the string is empty or whitespace-only, 0 otherwise.
+*/
 
 int	ft_isempty(char *str)
 {
@@ -94,6 +95,18 @@ int	ft_isempty(char *str)
 	return (1);
 }
 
+/*
+* Determines if a character is a whitespace character.
+*
+* Recognized whitespace: space, tab, newline, carriage return, vertical tab.
+*
+* Parameters:
+* - c: The character to check.
+*
+* Returns:
+* - 1 if the character is whitespace, 0 otherwise.
+*/
+
 int	ft_isspace(int c)
 {
 	if (c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\v')
@@ -102,70 +115,35 @@ int	ft_isspace(int c)
 		return (0);
 }
 
-int	quotes_checker(char *input, int len)
-{
-	int	in_quote;
-	int	i;
+/*
+* Duplicates a substring from `src` between indices `n` and `m` into
+* a new string.
+*
+* Parameters:
+* - src: The source string.
+* - n: The starting index (inclusive).
+* - m: The ending index (exclusive).
+*
+* Returns:
+* - A newly allocated substring, or NULL on failure.
+*/
 
-	i = 0;
-	in_quote = 0;
-	while (input && input[i] && i < len)
-	{
-		if (input[i] == '\'' || input[i] == '"')
-		{
-			if (in_quote == 0)
-				in_quote = input[i];
-			else if (in_quote == input[i])
-				in_quote = 0;
-		}
-		i++;
-	}
-	return (in_quote);
-}
-
-bool	builtin_cmd(char *cmd)
+char	*ft_strnmdup(char const *src, int n, int m)
 {
-	static char	*builtins[8];
 	int		i;
+	char	*dest;
 
 	i = 0;
-	builtins[0] = "exit";
-	builtins[1] = "cd";
-	builtins[2] = "env";
-	builtins[3] = "pwd";
-	builtins[4] = "unset";
-	builtins[5] = "export";
-	builtins[6] = "echo";
-	builtins[7] = NULL;
-	while (builtins[i])
+	if (!src || m == 0)
+		return (NULL);
+	dest = malloc(sizeof(char) * ((m - n) + 1));
+	if (!dest)
+		return (NULL);
+	while (src && src[i + n] && (i + n) < m)
 	{
-		if (ft_strncmp(cmd, builtins[i], ft_strlen(cmd)) == 0)
-			return (true);
+		dest[i] = src[i + n];
 		i++;
 	}
-	return (false);
-}
-
-char *ft_strjoin_free(char *s1, char *s2)
-{
-    char *joined;
-
-    joined = ft_strjoin(s1, s2);
-    free(s1);
-    return (joined);
-}
-
-void free_env(char **env)
-{
-    int i = 0;
-
-    if (env)
-    {
-        while (env[i])
-        {
-            free(env[i]);
-            i++;
-        }
-        free(env);
-    }
+	dest[i] = '\0';
+	return (dest);
 }
