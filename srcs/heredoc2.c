f3005944ca285808926b2d94b7d2c25228b6cc65
Thiwanka Somachandra <thivankas@outlook.com>
2025-05-20 03:22:16 +0300

    Change the handle_child_process in here_doc

--- srcs/heredoc2.c
+++ srcs/heredoc2.c
@@ -6,7 +6,7 @@
 /*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 15:35:42 by tsomacha          #+#    #+#             */
-/*   Updated: 2025/05/18 15:36:33 by tsomacha         ###   ########.fr       */
+/*   Updated: 2025/05/20 03:17:25 by tsomacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,11 @@ int	handle_child_process(t_shell *mini, t_redir *r, int fd)
 {
 	int	status;
 
+	status = 0;
 	init_sig_heredoc();
 	status = heredoc_interaction(mini, r, &fd);
 	close(fd);
-	exit(status);
+	return (status);
 }
 
 int	handle_heredoc_status(int status, char *cache, int fd, t_redir *r)
