2167fc652513b351e56330a4f11fcab9e48121e5
Thiwanka Somachandra <thivankas@outlook.com>
2025-05-20 05:50:26 +0300

    Fix file descriptor bug in heredoc

--- srcs/heredoc2.c
+++ srcs/heredoc2.c
@@ -6,7 +6,7 @@
 /*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 15:35:42 by tsomacha          #+#    #+#             */
-/*   Updated: 2025/05/20 03:17:25 by tsomacha         ###   ########.fr       */
+/*   Updated: 2025/05/20 05:49:57 by tsomacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,26 +20,22 @@ int	handle_child_process(t_shell *mini, t_redir *r, int fd)
 	init_sig_heredoc();
 	status = heredoc_interaction(mini, r, &fd);
 	close(fd);
-	return (status);
+	exit (status);
 }
 
 int	handle_heredoc_status(int status, char *cache, int fd, t_redir *r)
 {
-	if (WIFSIGNALED(status))
+	if (WEXITSTATUS(status))
 	{
 		close(fd);
 		unlink(cache);
+		free(r->filename);
+		r->filename = ft_strdup(cache);
 		free(cache);
 		return (130);
 	}
-	if (WEXITSTATUS(status) != 0)
-	{
-		close(fd);
-		unlink(cache);
-		free(cache);
-		return (WEXITSTATUS(status));
-	}
 	close(fd);
+	unlink(cache);
 	free(r->filename);
 	r->filename = ft_strdup(cache);
 	free(cache);
